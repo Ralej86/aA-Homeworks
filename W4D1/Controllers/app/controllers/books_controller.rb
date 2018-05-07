8d36e5d3760e6e94631461fc2e536b5cadd61c12
Ryan <alejandr257@gmail.com>
2018-05-06 19:40:00 -0700

    setup controllers for books

---
+++ W4D1/Controllers/app/controllers/books_controller.rb
@@ -0,0 +1,35 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    # your code here
+    render :index
+  end
+
+  def new
+    render :new
+    # your code here
+  end
+
+  def create
+    book = Book.new(book_params)
+    if book.save
+      redirect_to books_url
+    else
+      flash.now[:errors] = book.errors.full_messages
+      render :new
+    end
+    # your code here
+  end
+
+  def destroy
+    # your code here
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_url
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :author)
+  end
+end
